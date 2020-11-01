78a44920921ebc0022f629b9828414c8a5f5574f
Johan Gavelan <johangavelan@gmail.com>
2020-11-01 18:08:52 -0500

    accordion component done

---
+++ Accordion/app.js
@@ -0,0 +1,20 @@
+const headings = document.getElementsByClassName('accordion__heading')
+
+
+for(let heading of headings) {
+    heading.addEventListener('click', function() {
+        let details = this.nextElementSibling;
+        let icon = this.lastElementChild;
+        
+        if(details.style.maxHeight) {
+            icon.innerHTML = '+';
+            details.style.maxHeight = null;
+        }
+
+        else {
+            details.style.maxHeight = details.scrollHeight + 'px';
+            icon.innerHTML = '-';
+        }
+    })
+}
+
